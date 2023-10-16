789e50917f2cb1b80341d5d372822fc4d212b490
Maks Sushchuk <maks313455@gmail.com>
2023-10-16 13:03:46 +0300

    Add route fallback

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::fallback('404', function(){
+//     return view('error404');
+// });
