26e54c3724e70ed0ba95c8815f5fa593336694bb
nazarist <zn0923n@gmail.com>
2023-10-17 22:00:11 +0300

    fix tests and change workflows

--- tests/Feature/Http/Controllers/V1/Auth/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/V1/Auth/AuthControllerTest.php
@@ -3,6 +3,7 @@
 namespace Http\Controllers\V1\Auth;
 
 use App\Http\Controllers\V1\Auth\AuthController;
+use App\Http\Resources\V1\Auth\MeResource;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,13 +12,7 @@ use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
-    use DatabaseMigrations,
-        RefreshDatabase;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
+    use DatabaseMigrations;
 
     #[NoReturn]
     public function testSuccessfulLogin(): void
@@ -75,10 +70,10 @@ class AuthControllerTest extends TestCase
     public function testSuccessfulRegister(): void
     {
         $response = $this->postJson(route('auth.register'), [
-                'name'     => 'John Doe',
-                'email'    => 'email@gmail.com',
-                'password' => 'password'
-            ]);
+            'name'     => 'John Doe',
+            'email'    => 'email@gmail.com',
+            'password' => 'password'
+        ]);
 
 
         $response
@@ -91,18 +86,69 @@ class AuthControllerTest extends TestCase
     }
 
 
-//    public function testValidatorRegistrationFailed(): void
-//    {
-//        $response = $this->postJson(route('auth.register'), [
-//            'name'     => 'John',
-//            'email'    => 'invalid-email3',
-//            'password' => 'password'
-//        ]);
-//
-//        $response
-//            ->assertStatus(422)
-//            ->assertJsonStructure([
-//                'errors'
-//            ]);
-//    }
+    public function testValidatorRegistrationFailed(): void
+    {
+        $response = $this->postJson(route('auth.register'), [
+            'name'     => 'John',
+            'email'    => 'invalid-email3',
+            'password' => 'password'
+        ]);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'errors'
+            ]);
+    }
+
+
+
+    public function testSuccessfulLogout(): void
+    {
+        $user = User::factory()->create();
+
+        $token = auth()->attempt([
+            'email'    => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response = $this
+            ->withHeader('Authorization', "Bearer $token")
+            ->postJson(route('auth.logout'));
+
+        $response
+            ->assertOk()
+            ->assertJsonStructure([
+                'message'
+            ]);
+    }
+
+
+    public function testMe()
+    {
+        $user = User::factory()->create();
+
+        $token = auth()->attempt([
+            'email'    => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response = $this
+            ->withHeader('Authorization', "Bearer $token")
+            ->postJson(route('auth.me'));
+
+
+        $response
+            ->assertOk()
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'email_verified_at',
+                    'created_at',
+                    'updated_at'
+                ]
+            ]);
+    }
 }
