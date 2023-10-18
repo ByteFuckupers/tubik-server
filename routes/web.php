e9349afe16501119c342ca6da6595fdf054c852a
Maks Sushchuk <maks313455@gmail.com>
2023-10-18 19:46:08 +0300

    delete fallback

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::fallback('404', function(){
-    return view('error404');
-});
+// Route::fallback('404', function(){
+//     return view('error404');
+// });
